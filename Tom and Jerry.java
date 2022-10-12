542208abb6e9cc90bf67c37bc1d60fe694c1cf2b
saurabh chuadhary <supersaurabh017@gmail.com>
2022-10-12 17:51:53 +0530

    Create Tom and Jerry.java

---
+++ Tom and Jerry.java
@@ -0,0 +1,14 @@
+class Solution {
+    static int numsGame(int N) {
+        if(N%2==0)
+        { 
+            return 1;
+            
+        }
+        else 
+        {
+            return 0;
+            
+        }
+    }
+};
